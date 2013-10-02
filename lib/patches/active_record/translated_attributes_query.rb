81266b4fca794292905a10179127ab6f82c96cd6
Chris Salzberg <chrissalzberg@gmail.com>
2013-10-02 23:59:30 +0900

    add more specs and narrow relation to return matches only in this locale.

--- lib/patches/active_record/translated_attributes_query.rb
+++ lib/patches/active_record/translated_attributes_query.rb
@@ -6,7 +6,7 @@ ActiveRecord::QueryMethods.class_eval do
       translated_attribute_names = klass.translated_attribute_names
       if opts.is_a?(Hash) && (keys = opts.symbolize_keys.keys & translated_attribute_names).present?
         keys.each { |key| opts[klass.translations_table_name + '.' + key.to_s] = opts.delete(key) }
-        return klass.with_translations.where_without_translations(opts, *rest)
+        return klass.with_translations(Globalize.locale).where_without_translations(opts, *rest)
       end
     end
     where_without_translations(opts, *rest)
--- test/globalize3/translated_attributes_query_test.rb
+++ test/globalize3/translated_attributes_query_test.rb
@@ -1,8 +1,29 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-class TranslatedAttributesQuery < MiniTest::Spec
-  it 'finds record in translation table' do
-    post = Post.create(:title => 'a post title')
-    assert_equal Post.where(:title => 'a post title').first, post
+class TranslatedAttributesQueryTest < MiniTest::Spec
+  describe '.where' do
+    it 'finds record with matching attribute value in translation table' do
+      post = Post.create(:title => 'a title')
+      assert_equal Post.where(:title => 'a title').first, post
+    end
+
+    it 'only returns translations in this locale' do
+      Globalize.with_locale(:ja) { Post.create(:title => 'タイトル') }
+      assert Post.where(:title => 'タイトル').empty?
+    end
+
+    it 'chains relation' do
+      post = Post.create(:title => 'a title', :published => true)
+      Post.create(:title => 'another title', :published => false)
+      assert_equal Post.where(:title => 'a title', :published => true).load, [post]
+    end
+
+    it 'returns record with all translations' do
+      post = Post.create(:title => 'a title')
+      Globalize.with_locale(:ja) { post.update_attributes(:title => 'タイトル') }
+      post_by_where = Post.where(:title => 'a title').first
+      skip 'is this even possible?'
+      assert_equal post.translations, post_by_where.translations
+    end
   end
 end
